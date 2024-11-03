7c286520f17f4785651194d8f4cd1c4f46df5269
hawraa-3 <hawraaalmahdi.33@gmail.com>
2024-11-03 16:11:34 +0300

    submition

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+const heightInM= height/100;
+const BMI =weight/(heightInM*heightInM);
+alert(BMI);
+
+
+if (BMI < 18.5) {
+  alert(" underweight");
+
+} else if (BMI >= 18.5 && BMI < 25) {
+  alert( "healthy weight");
+
+} else if (BMI >= 25 && BMI < 30) {
+  alert("overweight.");
+
+} else  if( BMI >= 30) {
+  alert( "You are obese.");
+}
+
 }
